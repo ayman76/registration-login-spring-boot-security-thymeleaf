c48f40986a55c56bfc9d11c849bd1bad50a13bae
Ayman <a.m_ayman9@outlook.com>
2023-04-27 17:08:09 +0200

    Creating User and Roles Repos

---
+++ src/main/java/com/global/regLog/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.global.regLog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.global.regLog.entity.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long>{
+
+}
